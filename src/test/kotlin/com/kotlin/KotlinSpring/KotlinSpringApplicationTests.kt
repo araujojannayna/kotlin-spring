ac9e278f3ae425bb13b175fa4c6ffb15a8782bea
Jannayna Araujo <contatojannayna@gmail.com>
2022-06-24 21:05:07 -0300

    Starting repository

---
+++ src/test/kotlin/com/kotlin/KotlinSpring/KotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.KotlinSpring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
